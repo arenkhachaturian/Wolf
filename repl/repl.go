a98d19fd26be369361f3d4a7132c7b05b0ee3730
Aren Khachatoorian <khachatoorianaren@gmail.com>
2024-12-25 20:06:59 +0400

    Adding if statement and function statements

--- repl/repl.go
+++ repl/repl.go
@@ -7,14 +7,13 @@ import (
 	"fmt"
 	"io"
 	"local/wolf/lexer"
-	"local/wolf/token"
+	"local/wolf/parser"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -23,9 +22,19 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
