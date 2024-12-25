5b2b642a557f712218e40a92bf895a8df07ded96
Aren Khachatoorian <khachatoorianaren@gmail.com>
2024-12-26 02:31:47 +0400

    Adding evaluation support

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"local/wolf/evaluator"
 	"local/wolf/lexer"
 	"local/wolf/parser"
 )
@@ -28,8 +29,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
