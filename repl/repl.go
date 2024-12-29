e1c6658dd0ee903ca18f0ecbed11fec3c8b6ff20
Aren Khachatoorian <khachatoorianaren@gmail.com>
2024-12-29 23:26:38 +0400

    Support for storing and locating variables(identifiers)

--- repl/repl.go
+++ repl/repl.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"local/wolf/evaluator"
 	"local/wolf/lexer"
+	"local/wolf/object"
 	"local/wolf/parser"
 )
 
@@ -15,6 +16,8 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
+
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -29,7 +32,7 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
